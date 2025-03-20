e7f78fd3e07c5eb92b948289ad1df4eb430c437f
kosssst <kostyamed0003@gmail.com>
2025-03-20 12:05:23 +0200

    fixed security error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'FZXGLfBW12MWJ7k0TV8BTN7kVKiFbc7U2LVIyGy2XtNIO8k3Dj')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
