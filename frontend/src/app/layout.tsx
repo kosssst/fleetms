37a605f216c9bbe8742cbf86fa9d3a555d6e7189
kosssst <kostyamed0003@gmail.com>
2025-07-22 12:31:59 +0300

    changed favicon; added title

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,12 +2,15 @@
 
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-
+import { PROJECT_NAME} from "@/constants/appConfig";
 
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en" data-lt-installed="true" suppressHydrationWarning={true}>
+      <head>
+        <title>{PROJECT_NAME}</title>
+      </head>
       <body>
         <MantineProvider defaultColorScheme="dark">
           {children}
