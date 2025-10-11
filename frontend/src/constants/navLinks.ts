b9e79ffc977e231b422f026cd0541ed95d3a5102
kosssst <kostyamed0003@gmail.com>
2025-10-11 15:50:40 +0300

    added dashboard page

--- frontend/src/constants/navLinks.ts
+++ frontend/src/constants/navLinks.ts
@@ -1,5 +1,6 @@
 import { NavLink } from '@/types/common.types';
 
 export const navLinks: NavLink[] = [
+  { label: 'Dashboard', href: '/dashboard', authRequired: true },
   { label: 'Company', href: '/company', authRequired: true },
 ];
